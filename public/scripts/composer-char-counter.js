f0f22a6171184661575f1801904e763b93590a1b
Gokul Krishna <gokursx.123@gmail.com>
2024-05-25 16:03:54 -0700

    "Created composer-char-counter.js file"

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  $('#tweet-text').on('keyup', function(e) {
+   
+    //tracking character count input from user using keyup
+    const currCharCount = $(this).val().length;
+    
+    //locating the counter element
+    const counterElem = $(this).parent().children('.under-tweet-input').children('.counter');
+    const remainingChars = 140 - currCharCount;
+    
+    //updating counter elem on DOM to reflect current changes in remaining characters
+    counterElem.text(remainingChars);
+    if (remainingChars < 0) {
+      counterElem.css('color', 'mediumvioletred');
+    } else {
+      counterElem.css('color', '#545149');
+    }
+  });
+});
