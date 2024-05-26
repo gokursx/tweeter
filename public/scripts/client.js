d59a22eaa64a721c8715bed097fba2979dc92eef
Gokul Krishna <gokursx.123@gmail.com>
2024-05-26 11:54:09 -0700

    updated client.js with createTweetElement function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,82 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+// Test / driver code (temporary). Eventually will get this from the server.
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
 
+const $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+$('#tweets-container').append($tweet); //
+
+const createTweetElement = function(tweetData) {
+  const $tweet = $(`<article class="tweet">Hello world
+  
+<article class="tweet">
+      <header class="tweet-header">
+        <div class="user-profile">
+          <img class="user-icon" src="${tweetData.user.avatars}"></img> 
+          <h4 class="user-name">${tweetData.user.name}</h4>
+        </div>
+        <div>
+          <h4 class="user-handle">${tweetData.user.handle}</h4>
+        </div>
+      </header>
+      <div class="tweet-text">
+        ${escape(tweetData.content.text)}
+      </div>
+      <footer class="tweet-footer">
+        <span class="tweet-date">${timeago.format(tweetData.created_at)}</span>
+        <div class="tweet-response">
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+        </div>
+      </footer>
+    </article>`);
+  return $tweet;
+};
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+const renderTweets = function(tweets) {
+// loops through tweets
+// calls createTweetElement for each tweet
+// takes return value and appends it to the tweets container
+}
+
+renderTweets(data);
