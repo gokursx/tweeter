df8027177fadf432104fa393b008bc8d4c6f6ec5
Gokul Krishna <gokursx.123@gmail.com>
2024-05-26 09:49:30 -0700

    Added tweet container

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,21 +1,24 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // --- our code goes here ---
-  $('#tweet-text').on('keyup', function(e) {
-   
-    //counts the length of characters inthe box.
+
+  $('.new-tweet textarea').on('input', function (event) {
+    console.log("Input event on textarea within .new-tweet section, event:", event);
+
+    // Place your code here to handle the event
+    //counts the length of characters in the box.
     const currCharCount = $(this).val().length;
-    
+
     //Counter element locates the counter
     const counterElem = $(this).closest('form').find('.counter');
 
-    const remainingChars = 140 - currCharCount;
-    
-    //updating counter elem on DOM to reflect current changes in remaining characters
-    counterElem.text(remainingChars);
-    if (remainingChars < 0) {
-      counterElem.css('color', 'mediumvioletred');
+    const remChars = 140 - currCharCount;
+
+    //Displays color change depending upon characters in textbox
+    counterElem.text(remChars);
+    if (remChars < 0) {
+      counterElem.css('color', '#db751b');
     } else {
-      counterElem.css('color', '#545149');
+      counterElem.css('color', '#2d2c29');
     }
   });
 });
